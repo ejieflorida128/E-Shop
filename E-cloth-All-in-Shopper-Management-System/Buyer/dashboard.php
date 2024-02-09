832c98e8affec9bac65128ae01d466dc79ed85df
ejieflorida128 <ejieflorida128@gmail.com>
2024-02-09 12:06:11 +0800

    [ejie]
    ahua ge creatan ug dashboard php files and css

--- E-cloth-All-in-Shopper-Management-System/Buyer/dashboard.php
+++ E-cloth-All-in-Shopper-Management-System/Buyer/dashboard.php
@@ -1,70 +1,57 @@
 <?php
-include("../connection/conn.php");
+    include("../footer.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buyer Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="../Buyer_Design/dashboard.css">
+    <title>Dashboard</title>
+    <link rel = "stylesheet" href = "../Buyer_Design/dashboard.css">
+    <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body>  
+<body>
+      <div class="header">
+            <div class="logo">
+                <img src="../images/logo.png" alt="logo of the shop">
+            </div>
+            <div class="list">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">Transactions</a></li>
+                    <li><a href="#">About Us</a></li>
 
-    <div class="header">
-        <div class="logo">
-            <img src="../images/logo.png">
-        </div>
-        <div class="searchbar">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-            </svg>
-            <input type="text" name="searchhBar">
-        </div>
-        <div class="cart">
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-cart4" viewBox="0 0 16 16">
-                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
-            </svg>
-        </div>
-    </div>
+                </ul>
+            </div>
 
-    <div class="main">
-        <div class="content">
-            <?php
-            $getPic = "SELECT * FROM items";
-            $sqlForGetPic = mysqli_query($connForMyDatabase, $getPic);
-            $cardCount = 0;
-
-            while ($checkPicFromItemTable = mysqli_fetch_assoc($sqlForGetPic)) {
+            <a href = "../index.php" id = "logout">
+            <div class="logoutBtn">
                 
-                $item = $checkPicFromItemTable['img'];
-                echo "
-                <div class='card' style='width: 30rem; box-shadow: 0 4px 8px rgba(4, 4, 4, 1.1);'>
-                    <img src='$item'' class='card-img-top'>
-                    <div class='card-body'>
-                         <h4 class='card-title' style = 'display: flex; justify-content: center;'>12.8$</h4>
-                         <h6 class='card-title' style = 'display: flex; justify-content: center;'>Example Product</h6>
-                        <div class = 'buynow' style = 'display: flex; justify-content: center; margin-top: 10px;'>
-                        <a href='#' class='btn btn-primary' style = 'width: 300px;'>Add To Cart</a>
-                        </div>
-                      
-                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" fill="white" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                    </svg>
+                    <h5>LOGOUT</h5>
+                
+            </div>
+            </a>    
+      </div>
+      <div class="main">
+
+            <div class="textForDashboard">
+                <div class = "text">
+                      <span>Introducing<span> the E Cloth All-In Management System: a comprehensive solution designed to streamline and optimize various facets of organizational operations. This innovative platform combines cutting-edge technology with user-friendly interfaces to offer a seamless experience for businesses seeking enhanced efficiency and productivity. From inventory management to customer relations, financial tracking, and beyond, E Cloth provides a centralized hub for managing diverse tasks with ease. With customizable features tailored to meet specific business needs, this system empowers users to make informed decisions, improve collaboration, and drive sustainable growth. Welcome to the future of management solutions with E Cloth.
                 </div>
-                ";
-                $cardCount += 1;
-                
-                
-                 if ($cardCount % 2 == 0) {
-                    echo "</div><div class='row mt-5' style = 'style = 30rem; display: flex; justify-content: space-around;'>";
-                }
+                <div class="redirectToAboutUs">
+                    <a href="#">
+                        MORE DETAILS
+                    </a>
+                </div>
+            </div>
 
-                
-            }
-            ?>
-        </div>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+      </div>    
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </body>
 </html>
--- E-cloth-All-in-Shopper-Management-System/footer.php
+++ E-cloth-All-in-Shopper-Management-System/footer.php
@@ -6,9 +6,9 @@
     <title></title>
 </head>
 <body>
-        <div class = "footer">
+        <div class = "footer" style = "position: absolute; background-color: #365D1E; top: 94.9%; width: 100%; height:5%;">
                 <div class = "footerText" style = "display: flex; justify-content: center; position: relative;
-                top: 8px; letter-spacing: 5px; color: white;">
+                top: 8px; letter-spacing: 5px; color: white; font-size: 11px;">
                     E- Cloth All-In Shopper Management System - 2024 @ejieflorida128
                 </div>
         </div> 
