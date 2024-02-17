22a3d883ff842e8138b7140de86e39304bebf54e
ejieflorida128 <ejieflorida128@gmail.com>
2024-02-17 20:08:20 +0800

    [ejie]
    changes for February 17 2024

--- database/all_in_shopper.sql
+++ database/all_in_shopper.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 17, 2024 at 11:03 AM
+-- Generation Time: Feb 17, 2024 at 01:07 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -42,7 +42,8 @@ CREATE TABLE `buyer_account` (
 --
 
 INSERT INTO `buyer_account` (`id`, `username`, `password`, `profile_pic`, `fullname`, `age`, `location`) VALUES
-(10, 'athena', 'athena', '../profile_picture/ejie4.jpg', 'Athena Joy Barola Campania', 20, 'San Jose, Sogod Southern Leyte 6606 Atbang Emirates dapit Vet na Vet sulod lang sa kanto');
+(11, 'buyer1', 'buyer1', '../profile_picture/default.jpg', 'Buyer1', 20, 'I represent Buyer no. 1'),
+(12, 'buyer2', 'buyer2', '../profile_picture/default.jpg', 'Buyer 2', 19, 'I represent buyer no. 2');
 
 -- --------------------------------------------------------
 
@@ -52,6 +53,8 @@ INSERT INTO `buyer_account` (`id`, `username`, `password`, `profile_pic`, `fulln
 
 CREATE TABLE `cart_pending` (
   `id` int(11) NOT NULL,
+  `BuyerId` int(11) NOT NULL,
+  `SellerId` int(11) NOT NULL,
   `item_name` varchar(255) NOT NULL,
   `item_price` varchar(255) NOT NULL,
   `item_source` varchar(255) NOT NULL,
@@ -65,9 +68,15 @@ CREATE TABLE `cart_pending` (
 -- Dumping data for table `cart_pending`
 --
 
-INSERT INTO `cart_pending` (`id`, `item_name`, `item_price`, `item_source`, `buyer_fullname`, `buyer_location`, `buyer_age`, `seller`) VALUES
-(1, 'brown polo shirt for women', '10$', 'T-bunny Shirts', 'Athena Joy Barola Campania', 'San Jose, Sogod Southern Leyte 6606 Atbang Emirates dapit Vet na Vet sulod lang sa kanto', 20, 'Ejie Cabales Florida'),
-(3, 'Black Dress for women', '12$', 'T-bunny Shirts', 'Athena Joy Barola Campania', 'San Jose, Sogod Southern Leyte 6606 Atbang Emirates dapit Vet na Vet sulod lang sa kanto', 20, 'Ejie Cabales Florida');
+INSERT INTO `cart_pending` (`id`, `BuyerId`, `SellerId`, `item_name`, `item_price`, `item_source`, `buyer_fullname`, `buyer_location`, `buyer_age`, `seller`) VALUES
+(16, 11, 22, 'Biege Polo long sleeve for women', '9$', 'seller1', 'Buyer1', 'I represent Buyer no. 1', 20, 'Seller 1'),
+(17, 11, 22, 'Blue Polo long sleeve for women', '9$', 'seller1', 'Buyer1', 'I represent Buyer no. 1', 20, 'Seller 1'),
+(18, 11, 22, 'Blue Polo long sleeve for women', '9$', 'seller1', 'Buyer1', 'I represent Buyer no. 1', 20, 'Seller 1'),
+(19, 11, 23, 'Blue T-Shirt for women', '8$', 'Seller2', 'Buyer1', 'I represent Buyer no. 1', 20, 'Seller 2'),
+(20, 12, 22, 'Brown polo for women', '9$', 'seller1', 'Buyer 2', 'I represent buyer no. 2', 19, 'Seller 1'),
+(21, 12, 23, 'Blue T-Shirt for women', '8$', 'Seller2', 'Buyer 2', 'I represent buyer no. 2', 19, 'Seller 2'),
+(22, 11, 23, 'Blue T-Shirt for women', '8$', 'Seller2', 'Buyer1', 'I represent Buyer no. 1', 20, 'Seller 2'),
+(23, 11, 23, 'Blue T-Shirt for women', '8$', 'Seller2', 'Buyer1', 'I represent Buyer no. 1', 20, 'Seller 2');
 
 -- --------------------------------------------------------
 
@@ -77,6 +86,7 @@ INSERT INTO `cart_pending` (`id`, `item_name`, `item_price`, `item_source`, `buy
 
 CREATE TABLE `items` (
   `id` int(11) NOT NULL,
+  `SellerId` int(11) NOT NULL,
   `img` varchar(255) NOT NULL,
   `item_name` varchar(255) NOT NULL,
   `item_price` varchar(255) NOT NULL,
@@ -88,11 +98,14 @@ CREATE TABLE `items` (
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`id`, `img`, `item_name`, `item_price`, `item_source`, `seller`) VALUES
-(44, '../items/i4.jpg', 'brown polo shirt for women', '10$', 'T-bunny Shirts', 'Ejie Cabales Florida'),
-(45, '../items/i6.jpg', 'Black Dress for women', '12$', 'T-bunny Shirts', 'Ejie Cabales Florida'),
-(49, '../items/i11.jpg', 'Green T-Shirt for women', '9$', 'T-bunny Shirts', 'Ejie Cabales Florida'),
-(50, '../items/i17.jpg', 'Pink T-Shirt for women', '9$', 'JeansB', 'Ejie Cabales Florida');
+INSERT INTO `items` (`id`, `SellerId`, `img`, `item_name`, `item_price`, `item_source`, `seller`) VALUES
+(66, 22, '../items/i1.jpg', 'Biege Polo long sleeve for women', '9$', 'seller1', 'Seller 1'),
+(67, 22, '../items/i2.jpg', 'Blue Polo long sleeve for women', '9$', 'seller1', 'Seller 1'),
+(68, 22, '../items/i20.jpg', 'Light Brown T-Shirt for women', '9$', 'seller1', 'Seller 1'),
+(69, 22, '../items/i19.jpg', 'Light Brown T-Shirt for women', '9$', 'seller1', 'Seller 1'),
+(70, 22, '../items/i4.jpg', 'Brown polo for women', '9$', 'seller1', 'Seller 1'),
+(71, 23, '../items/i11.jpg', 'Green T-Shirt for women', '8$', 'Seller2', 'Seller 2'),
+(72, 23, '../items/i16.jpg', 'Blue T-Shirt for women', '8$', 'Seller2', 'Seller 2');
 
 -- --------------------------------------------------------
 
@@ -115,7 +128,8 @@ CREATE TABLE `seller_account` (
 --
 
 INSERT INTO `seller_account` (`id`, `username`, `password`, `profile_pic`, `fullname`, `bio`, `age`) VALUES
-(21, 'ejie', 'ejie', '../profile_picture/default.jpg', 'Ejie Cabales Florida', 'be Good Be Happy Everyday Code well seller', 19);
+(22, 'seller1', 'seller1', '../profile_picture/default.jpg', 'Seller 1', 'i represent seller 1', 20),
+(23, 'seller2', 'seller2', '../profile_picture/default.jpg', 'Seller 2', 'I represent Seller no. 2', 19);
 
 -- --------------------------------------------------------
 
@@ -137,11 +151,8 @@ CREATE TABLE `shop` (
 --
 
 INSERT INTO `shop` (`id`, `shop_name`, `shop_pic`, `shop_creator`, `shop_contactNo`, `shop_email`) VALUES
-(33, 'T-bunny Shirts', '../Store_Pic/defaultPic.jpg', 'ejie', '09091273962', 'ejieflorida001@gmail.com'),
-(34, 'JeansB', '../Store_Pic/defaultPic.jpg', 'ejie', '09123846123', 'ejieflorida002@gmail.com'),
-(35, 'PoloV', '../Store_Pic/defaultPic.jpg', 'ejie', '09123846123', 'ejieflorida003@gmail.com'),
-(36, 'b-biew', '../Store_Pic/defaultPic.jpg', 'ejie', '09123846123', 'ejieflorida004@gmail.com'),
-(37, 'c_yuppy', '../Store_Pic/defaultPic.jpg', 'ejie', '09123846123', 'ejieflorida005@gmail.com');
+(38, 'seller1', '../Store_Pic/defaultPic.jpg', 'seller1', '09934762718', 'seller1@gmail.com'),
+(39, 'Seller2', '../Store_Pic/defaultPic.jpg', 'seller2', '09123826739', 'seller2@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -185,31 +196,31 @@ ALTER TABLE `shop`
 -- AUTO_INCREMENT for table `buyer_account`
 --
 ALTER TABLE `buyer_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `cart_pending`
 --
 ALTER TABLE `cart_pending`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT for table `seller_account`
 --
 ALTER TABLE `seller_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `shop`
 --
 ALTER TABLE `shop`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
